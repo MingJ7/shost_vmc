55a1a043b26e759516dcbda30f7eb1589b10e014
MingJ7 <chiamingjiu@yahoo.com.sg>
2023-10-01 20:05:47 +0800

    swapped control direction for arms

--- components/MediaStreamPreview.tsx
+++ components/MediaStreamPreview.tsx
@@ -8,10 +8,10 @@ export function Preview ({mediaStream}: {mediaStream: MediaStream | undefined}){
     const [vmcStream, setVMCStream] = useState<VMCStreamer | undefined>()
     const videoRef = useRef(null)
     const [volume, setVolume] = useState(1);
-    const [armRotation, setArmRotation] = useState(-Math.PI/2);
+    const [armRotation, setArmRotation] = useState(Math.PI/4);
     
     function updateArmRotation(armRot: number){
-        vmcStream?.setArmRotation(armRot);
+        vmcStream?.setArmRotation(-armRot);
         setArmRotation(armRot);
     }
 
@@ -34,6 +34,7 @@ export function Preview ({mediaStream}: {mediaStream: MediaStream | undefined}){
             console.log("EffectVideo", videoElement);
             if(videoElement) {
                 const newVMCStream = new VMCStreamer(videoElement, mediaStream);
+                newVMCStream.setArmRotation(-armRotation);
                 setVMCStream(newVMCStream);
             }
             
@@ -56,7 +57,7 @@ export function Preview ({mediaStream}: {mediaStream: MediaStream | undefined}){
                 <div>
                     <AudioOutputSelector videoRef={videoRef}/>
                     <input type='range' max={1} min={0} step={0.01} onChange={(evt) => updateVolume(Number(evt.target.value))} value={volume}></input>
-                    <input type='range' max={0} min={-Math.PI*2} step={Math.PI/180} onChange={(evt) => updateArmRotation(Number(evt.target.value))} value={armRotation}></input>
+                    <input type='range' max={Math.PI*2} min={0} step={Math.PI/180} onChange={(evt) => updateArmRotation(Number(evt.target.value))} value={armRotation}></input>
                     <button onClick={togglePlay}>Toggle state</button>
                     <button onClick={(evt) => vmcStream.resetPose()}>Reset Pose</button>
                 </div>
